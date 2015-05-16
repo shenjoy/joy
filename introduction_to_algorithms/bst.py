e828fc85871a2b0c81d2cec5ec9410de694d13f8
shennian <shennian.joy@gmail.com>
2015-05-16 22:52:34 +0800

    update find min and next larger

--- introduction_to_algorithms/bst.py
+++ introduction_to_algorithms/bst.py
@@ -26,10 +26,14 @@ class BST():
             else:
                 node = node.right
 
-            if value < p.value:
-                p.left = Node(value)
-         else:
-             p.right = Node(value)
+        if value < p.value:
+            node = Node(value)
+            p.left = node
+            node.parent = p
+        else:
+            node = Node(value)
+            p.right = node
+            node.parent = p
 	
     def display(self):
         node = self.root
@@ -43,19 +47,46 @@ class BST():
 			
         show(node)
         print ""
-	
-	
-		
-		
+    
 
+    def search(self, k):
+        node = self.root
+        while node is not None:
+            if k < node.value:
+                node = node.left
+            elif k > node.value:
+                node = node.right
+            else:
+                break
+        return node
+     
+    def find_min(self, node):
+         while node.left is not None:
+             node = node.left
+         return node
+     
+    def next_larger(self, node):
+         if node.right is not None:
+             return self.find_min(node.right)
+         
+         p = node.parent
+         while p is not None and node is p.right:
+             node = p
+             p = p.parent
+         return p
+					
+					
 def test():
     nums = [7, 5, 9, 4, -3, 1]
     a = BST()
     for i in nums:
         a.insert(i)
     a.display()
-    a.insert(5)
+    a.insert(11)
     a.display()
+    n = a.search(4)
+    m = a.next_larger(n)
+    print m.value
 
 
 if __name__ == '__main__':
